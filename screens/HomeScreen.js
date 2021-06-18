80ec826832318da27fd353aab175ce7751586cc2
addy360 <abbyboy360@gmail.com>
2021-06-18 11:11:42 +0300

    input

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
 import Screen from "../components/Screen";
 
 const HomeScreen = () => {
   return (
     <Screen>
-      <View>
-        <Text>Hellow there</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.input}>
+            <AntDesign
+              style={styles.inputIcon}
+              name="search1"
+              size={20}
+              color="black"
+            />
+            <TextInput style={styles.inputText} placeholder="Seacrh ..." />
+          </View>
+        </View>
       </View>
     </Screen>
   );
@@ -14,4 +26,30 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 80,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  input: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    elevation: 1.5,
+    marginHorizontal: 20,
+
+    borderRadius: 0.5,
+    marginTop: 10,
+  },
+  inputText: {
+    flex: 1,
+  },
+  inputIcon: {
+    padding: 10,
+  },
+});
